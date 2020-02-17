117203afc475bab4e880ee212498c21a069873f9
haldryn <canales16086@iesmarenostrum.com>
2020-02-17 15:17:48 +0100

    Primer pull

---
+++ js/index.js
@@ -0,0 +1,182 @@
+//Recogemos en variables las etiquetas labels que serán rellenadas con los posibles errores en el formulario
+label_reg = document.getElementById("label_reg");
+label_ing = document.getElementById("label_ing");
+
+//Función que, mediante el uso de 3 expresiones regulares, valida los datos introducidos en los campos del formulario de registro. En caso de validación correcta, los datos del usuario pasan a registrarse en forma de cookie y se crea una variable de sesión con su nombre
+function validar() {
+
+    label_reg.innerHTML = "";
+
+    expreg_nombre = new RegExp(/^[A-Z]{3}$/);
+    nombre = document.getElementById("nom_usu").value
+    expreg_contra = new RegExp(/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.{6,})/);
+    contra = document.getElementById("passw").value;
+    expreg_correo = new RegExp(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/);
+    correo = document.getElementById("correo").value;
+
+    document.getElementById("nom_usu").style.borderColor = "";
+    document.getElementById("passw").style.borderColor = "";
+    document.getElementById("correo").style.borderColor = "";
+
+    fallos = 0;
+
+    if (!expreg_nombre.test(nombre)) {
+        document.getElementById("nom_usu").value = "";
+        document.getElementById("nom_usu").style.borderColor = "red";
+        fallos = fallos + 1;
+    }
+
+    if (!expreg_contra.test(contra)) {
+        document.getElementById("passw").value = "";
+        document.getElementById("passw").style.borderColor = "red";
+        fallos = fallos + 1;
+    }
+
+    if (!expreg_correo.test(correo)) {
+        document.getElementById("correo").value = "";
+        document.getElementById("correo").style.borderColor = "red";
+        fallos = fallos + 1;
+    }
+
+    if (fallos > 0) return false;
+    else {
+        var usu = document.getElementById("nom_usu").value;
+        if (buscar_usu_simple(usu) == false) {
+            createCookie(usu, usu);
+            createCookie("pass_" + usu, document.getElementById("passw").value);
+            createCookie("score_" + usu, 0);
+            sessionStorage.setItem("usuario", usu);
+            return true;
+        } else {
+            label_reg.innerHTML = "Usuario ya existente";
+            return false;
+        }
+    }
+}
+
+
+//Función que añade datos al archivo cookies que expiran en varios meses
+function createCookie(cookieName, cookieValue) {
+    document.cookie = cookieName + "=" + cookieValue + "; expires=Mon, 1 Jun 2020 12:00:00 UTC";
+}
+
+//Función que busca si un usuario existe o no en el archivo cookies
+function buscar_usu() {
+    label_ing.innerHTML = "";
+
+    nombre_usu = document.getElementById("nom_usu_reg").value;
+    contra_usu = document.getElementById("passw_usu_reg").value;
+
+    encontrado = false;
+    cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+
+        fin = cookies[i].indexOf("=");
+        nombre_cookie = cookies[i].substring(0, fin);
+        nombre_cookie = nombre_cookie.trim();
+
+        if (nombre_usu == nombre_cookie) {
+            if (buscar_contra(nombre_usu, contra_usu) == true) {
+                return true;
+            } else {
+                return false;
+            }
+            encontrado = true;
+        }
+    }
+
+    if (!encontrado) {
+        label_ing.innerHTML = "El usuario no existe";
+        return false
+    }
+}
+
+//Función similar a la anterior pero en cuanto encuentra si el usuario existe o no en las cookies devuelve un true o false
+function buscar_usu_simple(nombre_usu) {
+    encontrado = false;
+    cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+
+        fin = cookies[i].indexOf("=");
+        nombre_cookie = cookies[i].substring(0, fin);
+        nombre_cookie = nombre_cookie.trim();
+
+        if (nombre_usu == nombre_cookie) {
+            encontrado = true
+        }
+    }
+    if (encontrado == true) return true;
+    else return false;
+}
+
+//Función que a partir de un nombre de usuario y una contraseña, busca en el registro de cookies si ambos coinciden, en caso afirmativo, el formulario es validado y se crea una variable de sesión con el nombre del usuario
+function buscar_contra(nombre_usu, contra_usu) {
+    encontrado = false;
+    contra = "pass_" + nombre_usu + "=" + contra_usu;
+
+    cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+
+        if (cookies[i].trim() == contra) {
+            puntuaciones = document.cookie.split(";");
+            for (j = 0; j < puntuaciones.length; j++) {
+
+                fin = cookies[j].indexOf("=");
+                cookie_punt = cookies[j].substring(0, fin);
+                cookie_punt = cookie_punt.trim();
+                if (cookie_punt == "score_" + nombre_usu) {
+                    sessionStorage.setItem("usuario", usu);
+                }
+            }
+            encontrado = true;
+            return true;
+        }
+    }
+    if (!encontrado) {
+        label_ing.innerHTML = "Contraseña incorrecta";
+        return false;
+    }
+}
+
+//Funciones que filtran carácteres que son pasados por eventos de teclado
+function letra_usu(evento) {
+    evn_char = window.event.charCode || evento.charCode;
+    if (((evn_char <= 90) && (evn_char >= 65))) return true;
+    else return false;
+}
+
+function letra_pass(evento) {
+    evn_char = window.event.charCode || evento.charCode;
+    if (((evn_char <= 90) && (evn_char >= 65)) || ((evn_char <= 122) && (evn_char >= 97)) || ((evn_char <= 57) && (evn_char >= 48))) return true;
+    else return false;
+}
+
+
+//Función que muestra el formulario deseado: registrarse o volver a jugar
+function reg_ing(valor) {
+    switch (valor) {
+        case 1:
+            document.getElementById("reg").style.display = "none";
+            document.getElementById("ing").style.display = "inline";
+            break;
+        case 2:
+            document.getElementById("reg").style.display = "inline";
+            document.getElementById("ing").style.display = "none";
+            break;
+    }
+}
+
+//Función extra para comprobar que las cookies se guardaban correctamente durante las diversas fases de prueba
+function mostrar_cook() {
+    texto = "";
+    cookies = document.cookie.split(";");
+    if (cookies.length > 1) {
+        for (i = 0; i < cookies.length; i++) {
+            texto = texto + cookies[i].trim() + "\n";
+        }
+    } else texto = "No hay cookies que mostrar";
+    alert(texto)
+}
---
+++ js/main.js
@@ -0,0 +1,155 @@
+//Se asignan diferentes variables al inicio del juego y empiezan los intervalos
+usu = sessionStorage.getItem("usuario");
+puntos = 0;
+tiempo = 3000;
+cont = 0;
+cuentatras = [];
+
+puntuaciones = document.cookie.split(";");
+puntuacion = setInterval(sumarpuntos, 500);
+intervalo = setInterval(create_asteroid, tiempo);
+
+//Se accede a las coockies guardadas para conocer la mejor puntuación del jugador (0 en caso de nuevo jugador), mostrarla por pantalla y ser usada en cuanto la partida finalice
+for (j = 0; j < puntuaciones.length; j++) {
+
+    fin = puntuaciones[j].indexOf("=");
+    cookie_punt = puntuaciones[j].substring(0, fin);
+    cookie_punt = cookie_punt.trim();
+
+    if (cookie_punt == "score_" + usu) {
+        puntos_base = puntuaciones[j].substring(puntuaciones[j].indexOf("=") + 1, puntuaciones[j].length);
+        document.getElementById("puntuacion_previa").innerHTML = "Puntuación anterior: " + puntos_base;
+    }
+}
+
+//Función que, mediante el evento onmouseover en el body, asigna un ancho y un alto a un par de divs que hacen el efecto de mirilla para apuntar
+function position(x) {
+    evn_web_x = window.event.clientX || x.clientX;
+    evn_web_y = window.event.clientY || x.clientY;
+
+    document.getElementById("xaxis").style.width = evn_web_x + "px";
+    document.getElementById("yaxis").style.height = evn_web_y + "px";
+}
+
+//Función que crea asteroides; estos son imágenes con animación que son situados en un lugar aleatorio dentro del body. Cada asteroide tiene asignado un id y mediante variables dinámicas una cuenta atrás cuya única manera de desactivarla es pulsando sobre la imagen
+function create_asteroid() {
+
+    cont = cont + 1;
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    var randomW = Math.floor(Math.random() * w);
+    var randomH = Math.floor(Math.random() * h);
+
+    var asteroid = document.createElement("IMG");
+    asteroid.setAttribute("src", "img/asteroid.png");
+    asteroid.setAttribute("id", cont);
+    document.body.appendChild(asteroid);
+
+    document.getElementById(cont).style.top = randomH + "px";
+    document.getElementById(cont).style.right = randomW + "px";
+    document.getElementById(cont).onclick = function() { delete_asteroid(this); };
+
+
+    cuentatras[cont] = window.setTimeout(function() {
+        pausar();
+        setTimeout(() => {
+            game_over();
+        }, 100);
+    }, 5000);
+}
+
+//Función que elimina el asteroide y su contador
+function delete_asteroid(objeto) {
+    asteroid = document.getElementById(objeto.id);
+    asteroid.parentNode.removeChild(asteroid);
+    clearTimeout(cuentatras[objeto.id]);
+}
+
+//Función que espera la entrada de las letra P (para activar la función pausar) y R (para la función reanudar)
+function letra(evento) {
+    evn_keycode = window.event.keyCode || evento.keyCode;
+    evn_code = String.fromCharCode(evn_keycode)
+
+    if (evn_keycode == 80) {
+        pausar();
+    }
+
+    if (evn_keycode == 82) {
+        reanudar();
+    }
+}
+
+//Función que detiene las animaciones y los intervalos, además muestra un cartel al usuario
+function pausar() {
+    document.getElementById("pausado").style.display = "inline";
+    document.getElementById("separador").style.display = "none";
+    clearInterval(puntuacion);
+    clearInterval(intervalo);
+    asteroids = document.getElementsByTagName("img");
+    for (i = 0; i < asteroids.length; i++) {
+        asteroid = asteroids[i];
+        clearInterval(cuentatras[asteroid.id])
+        asteroid.style.animationPlayState = "paused";
+    }
+    pause = true;
+}
+
+//Función que reanuda las animaciones y los intervalos
+function reanudar() {
+    if (pause == true) {
+        document.getElementById("pausado").style.display = "none";
+        document.getElementById("separador").style.display = "none";
+        puntuacion = setInterval(sumarpuntos, 500);
+        intervalo = setInterval(create_asteroid, tiempo);
+        asteroids = document.getElementsByTagName("img")
+        for (i = 0; i < asteroids.length; i++) {
+            asteroid = asteroids[i];
+            cuentatras[asteroid.id] = window.setTimeout(function() {
+                pausar();
+                game_over();
+            }, 5000);
+            asteroid.style.animationPlayState = "running";
+        }
+    }
+}
+
+//Función que, en relación al tiempo transcurrido, va sumando puntos; cuando llega a ciertas cantidades acelera el tiempo de los intervalos de creación de asteroides para aumentar su dificultad
+function sumarpuntos() {
+    puntos = puntos + 1;
+    document.getElementById("puntuacion_actual").innerHTML = "Puntos: " + puntos;
+    if (puntos == 20) {
+        tiempo = 2000;
+        clearInterval(intervalo)
+        intervalo = setInterval(create_asteroid, tiempo);
+    }
+
+    if (puntos == 50) {
+        tiempo = 800;
+        clearInterval(intervalo)
+        intervalo = setInterval(create_asteroid, tiempo);
+    }
+
+    if (puntos == 70) {
+        tiempo = 450;
+        clearInterval(intervalo)
+        intervalo = setInterval(create_asteroid, tiempo);
+    }
+}
+
+//Función que detiene el conteo de puntos y comprueba la puntuación final: de ser mayor a la máxima del usuario la registra como su mejor record. Tras unos segundos vuelve a la pantalla de inicio
+function game_over() {
+    document.getElementById("imagen_rota").style.display = "inline"
+    clearInterval(puntuacion);
+    document.getElementById("game_over").style.display = "inline";
+
+    if (puntos_base < puntos) {
+        document.cookie = "score_" + usu + "=" + puntos + "; expires=Mon, 1 Jun 2020 12:00:00 UTC";
+        document.getElementById("game_over").innerHTML = "<h1>HAS PERDIDO</h1><br><h3>NUEVO RECORD REGISTRADO</h3>";
+    } else {
+        document.getElementById("game_over").innerHTML = "<h1>HAS PERDIDO</h1><br><h3>NO HAS SUPERADO TU MEJOR RECORD</h3>";
+    }
+    setTimeout(() => {
+        window.location.href = "index.html";
+    }, 5000);
+}
